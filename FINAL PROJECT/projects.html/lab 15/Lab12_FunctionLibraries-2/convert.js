13b531312964d8ca2a0d9c6c5aba1a78d405dedd
Elian1412 <89876904+Elian1412@users.noreply.github.com>
2021-12-12 15:26:42 -0800

    Add files via upload

---
+++ FINAL PROJECT/projects.html/lab 15/Lab12_FunctionLibraries-2/convert.js
@@ -0,0 +1,113 @@
+// convert.js				Dave Reed
+// Functions for converting between English and Metric values
+///////////////////////////////////////////////////////////////
+
+function InchesToCentimeters(inches)
+// Assumes: inches is a distance, measured in inches
+// Returns: the corresponding distance in centimeters
+{
+  var cm;
+
+  cm = inches * 2.54;
+  return cm;
+}
+function CentimetersToInches(cm)
+// Assumes: cm is a distance, measured in centimeters
+// Returns: the corresponding distance in inches
+{
+  var inches;
+inches = cm / 2.54;
+  return inches;
+}
+
+function KilogramsToPounds(kg)
+// Assumes: kg is a weight, measured in kilograms
+// Returns: the corresponding weight in pounds
+{
+ var pounds;
+pounds = kg * 2.205;
+ return pounds;
+}
+
+function PoundsToKilograms(pounds)
+// Assumes: pounds is a weight, measured in pounds
+// Returns: the corresponding weight in kilograms
+{
+ var kg;
+kg = pounds/ 2.205
+ return kg;
+}
+
+function SquaremetersToSquarefeet(sm)
+// Assumes: sm is a area, measured in square meters
+// Returns: the corresponding area in square feet
+{
+ var sf;
+sf = sm * 10.764
+ return sf;
+}
+
+function SquarefeetToSquaremeters(sf)
+// Assumes: sf is a area, measured in square-feet
+// Returns: the corresponding weight in square-meters
+{
+ var sm;
+sm = sf/ 10.764
+ return sm;
+}
+
+function InchesToCentimeters(inches)
+// Assumes: inches is a distance, measured in inches
+// Returns: the corresponding distance in centimeters
+{
+  var cm;
+cm = inches * 2.54;
+  return cm;
+}
+
+function CentimetersToInches(cm)
+// Assumes: cm is a distance, measured in centimeters
+// Returns: the corresponding distance in inches
+{
+  var inches;
+inches = cm / 2.54;
+  return inches;
+}
+
+function KilogramsToPounds(kg)
+// Assumes: kg is a weight, measured in kilograms
+// Returns: the corresponding weight in pounds
+{
+ var pounds;
+pounds = kg * 2.205;
+ return pounds;
+}
+
+function PoundsToKilograms(pounds)
+// Assumes: pounds is a weight, measured in pounds
+// Returns: the corresponding weight in kilograms
+{
+ var kg;
+kg = pounds/ 2.205
+ return kg;
+}
+
+function SquaremetersToSquarefeet(sm)
+// Assumes: sm is a area, measured in square meters
+// Returns: the corresponding area in square feet
+{
+ var sf;
+ sf = sm * 10.764
+ return sf;
+}
+
+function SquarefeetToSquaremeters(sf)
+// Assumes: sf is a area, measured in square-feet
+// Returns: the corresponding weight in square-meters
+{
+ var sm;
+ sm = sf/ 10.764
+ return sm;
+}
+
+
